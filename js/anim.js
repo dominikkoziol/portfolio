13ede94c0a94c919669bf209378fe4d37b6d9899
dominikkoziol <dkoziol@interia.com>
2017-06-14 12:40:47 +0200

    Add files via upload

--- js/anim.js
+++ js/anim.js
@@ -1,6 +1,12 @@
 $( "#logo" ).one( "click", function() {
- 	$("#header").animate({marginRight: '100%', marginTop: '35vh'});
-	$("#logo").css({height: '25vh'});
+	"use strict";
+ 	$("#header").animate({marginRight: '91%', marginTop: '36vh'});
+	$("#logo").css({height: '26vh'});
+	window.setTimeout(function() {
+    	$("#menu-top").css({display: 'block'});
+		$("#menu-bottom").css({display: 'block'});
+    }, 850);
+	
 });
 
 
